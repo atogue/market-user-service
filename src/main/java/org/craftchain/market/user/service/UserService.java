be80c13a8120d03baf633d518c28917bfcf6a75b
Anicet Togue <atogue@gmail.com>
2021-01-13 14:12:25 +0100

    wire rest template and define rest call to order service

--- src/main/java/org/craftchain/market/user/service/UserService.java
+++ src/main/java/org/craftchain/market/user/service/UserService.java
@@ -1,18 +1,31 @@
 package org.craftchain.market.user.service;
 
+import org.craftchain.market.user.common.TransactionRequest;
+import org.craftchain.market.user.common.TransactionResponse;
 import org.craftchain.market.user.entity.User;
 import org.craftchain.market.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private RestTemplate template;
 
-    public User createUser(User user) {
-        return repository.save(user);
+    public TransactionResponse createUser(TransactionRequest request) {
+        User user = request.getUser();
+        request.getOrder().setClientId(user.getUserId());
+        // rest call to order service
+        TransactionResponse response = template.postForObject("http://localhost:9192/order/bookOrder", request, TransactionResponse.class);
+        repository.save(user);
+        if (response != null) {
+            response.setUser(user);
+        }
+        return response;
     }
 
 }
